7b1dc6facbb45961c5464b6337ab033363eacc3c
nicewoong <thoughtscode@gmail.com>
2016-11-22 21:17:03 +0900

    Modify view controllers
    - 탭바뷰 컨트롤러 내에 있는 두 개의 뷰컨트롤러를 삭제
    - 뷰컨트롤러 대신 TableViewController 를 생성해서 추가했음.

---
+++ pleet/NearListCell.swift
@@ -0,0 +1,24 @@
+//
+//  NearListCell.swift
+//  pleet
+//
+//  Created by woongbook on 2016. 11. 22..
+//
+//
+
+import UIKit
+
+class NearListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
