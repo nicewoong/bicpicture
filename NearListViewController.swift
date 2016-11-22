a4a8ea69cd8d35ad874bc1fbb06321c85f0b49e5
nicewoong <thoughtscode@gmail.com>
2016-11-22 22:10:33 +0900

    Insert test data to NearListTableVIew
    - NearList 에 테스트 데이터 세개 넣어서 확인완료.
    - 이후 리스트의 cell 클릭시 새로운 뷰로 이동하는 것 작업예

--- NearListViewController.swift
+++ NearListViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class NearListViewController: UITableViewController {
 
+    var currentNearUserList:[[String:String]] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,6 +21,16 @@ class NearListViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        /* make test data */
+        let firstPerson = ["profileImage":"firstProfileImage.png", "profileName":"firstNema", "profileSimpleMessage":"fisrtMessage"]
+        let secondPerson = ["profileImage":"firstProfileImage.png", "profileName":"secondName", "profileSimpleMessage":"secondMessage"]
+        let thirdPerson = ["profileImage":"firstProfileImage.png", "profileName":"thirdNema", "profileSimpleMessage":"thirdMessage"]
+        
+        currentNearUserList.append(firstPerson)
+        currentNearUserList.append(secondPerson)
+        currentNearUserList.append(thirdPerson)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,25 +40,31 @@ class NearListViewController: UITableViewController {
 
     // MARK: - Table view data source
 
+    /*table view의 섹션. -> override 안되어 있으면 default 값인 1 로 정해짐.
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0
-    }
+    }*/
 
+    /* row의 갯수 즉, 아이템 갯수를 반환해줘야 합니다. 
+     -> 데이터 리스트의 크기를 반환해주면 될 것임 */
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return currentNearUserList.count
     }
 
-    /*
+    /* 셀(아이템 카드뷰)의 데이터를 채워 넣어 줍니다. */
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as! NearListCell // 뷰를 재활용 하기 위해 큐에서 놀고 있는
 
         // Configure the cell...
-
+        cell.profileImageView.image = UIImage(named: "fisrtProfileImage.png")
+        cell.profileUserName.text = currentNearUserList[indexPath.row]["profileName"]
+        cell.profileSimpleMessage.text = currentNearUserList[indexPath.row]["profileSimpleMessage"]
+        
         return cell
     }
-    */
+ 
 
     /*
     // Override to support conditional editing of the table view.
--- pleet/NearListCell.swift
+++ pleet/NearListCell.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
+/*
+ NearList Table view 에 나타나는 주변사용자 카드(Cell) 입니다.
+ */
 class NearListCell: UITableViewCell {
 
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var profileUserName: UILabel!
+    @IBOutlet weak var profileSimpleMessage: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        profileImageView.layer.cornerRadius = profileImageView.frame.size.height/2
+        profileImageView.layer.masksToBounds = true
+        profileImageView.layer.borderWidth = 0
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -21,4 +33,5 @@ class NearListCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    
 }
